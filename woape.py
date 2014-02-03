017a8e3cbb0d79ada7a37c00e4b925bd213b83f1
Nikolay Vasiliev <7804425+lonlylocly@users.noreply.github.com>
2014-02-03 22:48:35 +0400

    fetch tweets by date window; prefer most talkie users

--- woape.py
+++ woape.py
@@ -7,6 +7,7 @@ import re
 import time
 from sets import Set
 import sys,codecs
+from datetime import datetime, timedelta
 
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
@@ -17,6 +18,14 @@ headers = {"Authorization":"Bearer AAAAAAAAAAAAAAAAAAAAAEBgVAAAAAAAxZcUIQhxg"+
 #c.request('GET', '/1.1/statuses/show.json?id=%s'%tweet_id, '', headers)
 
 CHAINS_GOAL = 1000000
+TWEETS_START_DAY = datetime.now() - timedelta (days = 40)
+DB_FILENAME = 'more_replys2.db'
+
+class WoapeException(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
 
 def create_tables(cur):
     cur.execute("""
@@ -27,6 +36,7 @@ def create_tables(cur):
             username text,
             in_reply_to_username text,
             in_reply_to_id integer,
+            created_at text,
             PRIMARY KEY (id)
         )
     """)
@@ -35,23 +45,24 @@ def create_tables(cur):
         (
             username text,
             user_done integer default 0,
+            reply_cnt integer default 0,
             PRIMARY KEY (username)
         )
     """)
 
-def save_reply(cur, reply, username):
+def save_tweet(cur, reply, username):
     try:
         cur.execute("""
             INSERT OR IGNORE INTO tweets
-            VALUES (?, ?, ?, ?, ?)
-        """, (reply["id"], reply["text"], username, reply["in_reply_to_screen_name"], reply["in_reply_to_status_id"]))
+            VALUES (?, ?, ?, ?, ?, ?)
+        """, (reply["id"], reply["text"], username, reply["in_reply_to_screen_name"], reply["in_reply_to_status_id"], reply["created_at"]))
     except Exception as e:
         print "[ERROR] %s "  % e
 
 def get_path(path):
     c = httplib.HTTPSConnection('api.twitter.com')
     c.set_debuglevel(0)
-    print "Open path: %s" % path
+    print "[%s] Open path: %s" % (time.ctime(), path)
     resp = c.request('GET', path, '', headers)
     return c.getresponse()
 
@@ -90,44 +101,88 @@ def try_several_times(f, times, error_return=[]):
         try:
             res = f()
             return res
+        except WoapeException as e:
+            print "[%s] Stop trying, WoapeException: %s" % (time.ctime(), e)
+            break
         except Exception as e:
             print "[%s] [ERROR] %s" % (time.ctime(), e)
     return error_return
 
+def get_tw_create_time(t):
+    create_time = t["created_at"]
+    create_time = re.sub("[+-]\d\d\d\d", "", create_time)
+    dt = datetime.strptime(create_time, "%a %b %d %H:%M:%S  %Y")
+    
+    return dt
+
+def get_more(cur, username, maxid=None):
+    resp = None
+    if maxid is None:
+        resp = get_path('/1.1/statuses/user_timeline.json?screen_name=%s&count=200'%username)
+    else:
+        resp = get_path('/1.1/statuses/user_timeline.json?screen_name=%s&max_id=%s&count=200'%(username,maxid))
+    if resp.status is not 200:
+        print "[ERROR] Response code: %s %s. Response body: %s"% (resp.status, resp.reason, resp.read() )
+        mark_user_done(cur, username)
+        raise WoapeException('Invalid response')        
+
+    return resp
+
+
 def iteration(cur, username, replys_only=False):
     if username is None:
-        print "[ERROR] got None username"
-        return []
+        raise WoapeException('Username is none')
 
     users_seen = fetch_list(cur, "select username from users") 
     new_users = []
     print "[%s] Start loop iteration: %s" % (time.ctime(), username)
-    
-    resp = get_path('/1.1/statuses/user_timeline.json?screen_name=%s&count=200'%username)
-    if resp.status is not 200:
-        print "[ERROR] Response code: %s %s. Response body: %s"% (resp.status, resp.reason, resp.read() )
-        mark_user_done(cur, username)
-        return []
 
-    content = resp.read()
-
+    oldest_tweet_time = datetime.now()
+    minid = None
     cnt = 0
-    for t in json.loads(content):
-        if replys_only and t["in_reply_to_status_id"] is None :
-            continue
-        if not got_russian_letters(t["text"]):
-            continue
-        
-        #print "Got reply-tweet: %s '%s'" % (t["id"], t["text"])
-        save_reply(cur, t, username)
-        cnt = cnt + 1
 
-        fellow = t["in_reply_to_screen_name"]
-        if fellow is not None and fellow not in users_seen and fellow not in new_users:
-            cur.execute("insert or ignore into users (username) values (?)", (fellow, ))
-            new_users.append(fellow)
-    
+    while oldest_tweet_time > TWEETS_START_DAY:
+        resp = get_more(cur, username, minid) 
+
+        content = resp.read()
+        last_minid = minid
+
+        conv_partners = {}
+        for t in json.loads(content):
+            ct = get_tw_create_time(t) 
+            if ct < oldest_tweet_time:
+                oldest_tweet_time = ct
+            if ct < TWEETS_START_DAY:
+                oldest_tweet_time = ct
+                break
+            if replys_only and t["in_reply_to_status_id"] is None :
+                continue
+            if not got_russian_letters(t["text"]):
+                continue
+            if minid is None or minid > int(t["id"]):
+                minid = int(t["id"])
+ 
+            #print "Got reply-tweet: %s '%s'" % (t["id"], t["text"])
+            save_tweet(cur, t, username)
+            cnt = cnt + 1
+
+            fellow = t["in_reply_to_screen_name"]
+            if fellow not in conv_partners:
+                conv_partners[fellow] = 0
+            conv_partners[fellow] = conv_partners[fellow] + 1
+            if fellow is not None and fellow not in users_seen and fellow not in new_users:
+                cur.execute("insert or ignore into users (username) values (?)", (fellow, ))
+                new_users.append(fellow)
+
+        for partner in conv_partners:
+            cur.execute("update users set reply_cnt = reply_cnt + ? where username = ?", (conv_partners[partner], partner))
+
+        if minid is None or minid == last_minid:
+            print "[%s] cannot get valid minid" % (time.ctime())
+            break    
     mark_user_done(cur, username)
+
+    print "[%s] Oldest tweet time: %s " % (time.ctime(), oldest_tweet_time)
     
     print "Saved %s tweets" % cnt
 
@@ -136,14 +191,14 @@ def iteration(cur, username, replys_only=False):
 
 def main():
     print "[%s] Startup" % time.ctime()
-    con = sqlite3.connect('replys.db')
+    con = sqlite3.connect(DB_FILENAME)
     con.isolation_level = None
 
     cur = con.cursor()
     create_tables(cur)
 
     while True:
-        users = fetch_list(cur, "select username from users where user_done = 0 limit 100")
+        users = fetch_list(cur, "select username from users where user_done = 0 order by reply_cnt desc limit 1")
     
         if len(users) == 0:
             print "No users left"
@@ -155,47 +210,20 @@ def main():
         talked_to_users = try_several_times(f, 3, [])
        
         # try best to get full chains  
-        for talked_to_user in talked_to_users:
-            if talked_to_user == username:
-                continue
-            f2 = lambda : iteration(cur, talked_to_user)
-            try_several_times(f2, 3)
+        #for talked_to_user in talked_to_users:
+        #    if talked_to_user == username:
+        #        continue
+        #    f2 = lambda : iteration(cur, talked_to_user)
+        #    try_several_times(f2, 3)
 
         chains = get_chains(cur)     
         if len(chains) >= CHAINS_GOAL:
             print "Got enough chains, breaking"
             break
+        else:
+            print "Has %s chains, need %s, continue" % (len(chains), CHAINS_GOAL)
 
 
-def old_main():
-    while len(users) > 0 and len(users_done) < 100:
-        username = users[0]
-        print "Start loop iteration: %s" % username
-        print users
-        users_done.append(username)
-        
-        resp = get_path('/1.1/statuses/user_timeline.json?screen_name=%s&count=200'%username)
-        if resp.status is not 200:
-            print "[ERROR] Response code: %s %s. Response body: %s"% (resp.status, resp.reason, resp.read() )
-            continue
-
-        new_users = []
-        content = resp.read()
-
-        for t in json.loads(content):
-            if t["in_reply_to_status_id"] is not None and got_russian_letters(t["text"]):
-                print "Got reply-tweet: %s '%s'" % (t["id"], t["text"])
-                save_reply(cur, t, username)
-                con.commit()
-
-                fellow = t["in_reply_to_screen_name"]
-                if fellow not in users_seen:
-                    new_users.append(fellow)
-                    users_seen.append(fellow)
-                    cur.execute("insert or ignore into users (username) values (?)", (fellow))
-        
-        users = users + new_users
-        cur.execute("update users set user_done = 1 where username = ?", (username))
     
 if __name__ == "__main__":
     main()
