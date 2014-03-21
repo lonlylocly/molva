ab252c4ce8a7b6156f8cabb80fa8d5fea6c1d7da
Nikolay Vasiliev <7804425+lonlylocly@users.noreply.github.com>
2014-03-21 21:55:35 +0400

    get tomita more accurate

--- tomita/first.cxx
+++ tomita/first.cxx
@@ -1,3 +1,13 @@
 #encoding "utf-8"    // сообщаем парсеру о том, в какой кодировке написана грамматика
 
-S -> Noun interp (SimpleFact.Noun);           // правило, выделяющее цепочку, состоящую из одного существительного
+// не разрешаем омонимы (разные части речи)
+S -> Noun<no_hom> interp (SimpleFact.Noun);
+
+// омонимы только для согласованных существительных
+S -> Adj<gnc-agr[1]> Noun<gnc-agr[1]> interp (AdjNounFact.Noun);
+
+S -> Noun<gnc-agr[1]> interp (AdjNounFact.Noun) Adj<gnc-agr[1]> ;
+
+S -> Noun<rt, sp-agr[2]> interp (VerbNounFact.Noun) Verb<sp-agr[2]>;
+
+S -> Verb<sp-agr[2]> Noun<rt, sp-agr[2]> interp (VerbNounFact.Noun) ;
--- tomita/q2.sql
+++
@@ -1,3 +0,0 @@
-select noun_md5
-from nouns
-where noun in ('а', 'нет', 'у', 'уж', 'ли', 'в', 'до', 'я', 'и', 'с'); 
