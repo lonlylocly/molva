fdfe7f49f25002074130a24b15ecdcef044bc995
Nikolay Vasiliev <7804425+lonlylocly@users.noreply.github.com>
2014-09-06 01:25:38 +0400

    refactor css,js,html

--- web/js/clusters.js
+++ web/js/clusters.js
@@ -1,4 +1,3 @@
-
 function getCurUrlParams() {
     var cur_url = "" + document.URL;
     var url_parts = cur_url.split("?");
@@ -25,12 +24,18 @@ function get_max_trend(cluster) {
     return trend_vals[0];
 }
 
-function set_trend_color(cluster) {
+function get_avg_trend(cluster) {
     var avg_trend = 0;
     for(var i=0; i<cluster["members"].length; i++) {
         avg_trend += parseFloat(cluster["members"][i]["trend"]);
     }
     avg_trend = avg_trend / cluster["members"].length;
+
+    return avg_trend;
+}
+
+function set_trend_color(cluster) {
+    var avg_trend = get_avg_trend(cluster);
 
     var trend_class = "trend-unknown";
     var sign = "";
@@ -54,7 +59,37 @@ function goBack() {
     window.location.assign("/?skip=" + parseInt(skip + 1))
 }
 
-function loadClusters(parse_url_args) {
+function compare_max_trend (a,b) {
+    var a_val = get_max_trend(a);
+    var b_val = get_max_trend(b);
+
+    if (a_val < b_val) return -1;
+    if (a_val > b_val) return 1;
+    return 0;
+}
+
+function fill_cluster_properties(cl) {
+    var cl2= [];
+    for(var i=0; i<cl.length; i++) {
+        var mems = $.map(cl[i]["members"], function(l) {
+            return l["text"];
+        })
+
+        try {
+            set_trend_color(cl[i]);
+        } catch(e) {
+            console.log(e);
+        }
+
+        if ( cl[i]["members_len"] > 1) {
+            cl2.push(cl[i]);
+        }
+    }
+
+    return cl2;
+}
+
+function loadClusters() {
     var skip = parseInt(getCurUrlParams()["skip"]);
     
     if (isNaN(skip)) {
@@ -64,56 +99,21 @@ function loadClusters(parse_url_args) {
     console.log("loadClusters");
     $.get( "/api/cluster?skip=" + skip, function( data ) {
         try{
-        var resp = JSON.parse(data);
-    
-        var cl = resp["clusters"];
+            var resp = JSON.parse(data);
+        
+            var cl = resp["clusters"];
 
-        cl.sort(function(a,b) {
-            var a_val = get_max_trend(a);
-            var b_val = get_max_trend(b);
+            cl.sort(compare_max_trend).reverse();
 
-            if (a_val < b_val) return -1;
-            if (a_val > b_val) return 1;
-            return 0;
-        }).reverse();
+            var limit = 30;
 
-        var limit = 30;
+            var cl2 = fill_cluster_properties(cl); 
 
-        var cl2= [];
-        for(var i=0; i<cl.length; i++) {
-            var mems = $.map(cl[i]["members"], function(l) {
-                return l["text"];
-            })
+            var source = $("#cluster-template").html();
+            var template = Handlebars.compile(source);
 
-            try {
-                set_trend_color(cl[i]);
-            } catch(e) {
-                console.log(e);
-            }
+            $( "#cluster-holder" ).html( template({"groups": cl2.slice(0, 20), "update_time": resp["update_time"]}) );
 
-            cl[i]["query_string"] = mems.join("+"); 
-            if ( cl[i]["members_len"] > 1) {
-                cl2.push(cl[i]);
-            }
-        }
-        cl = cl2.slice(0, 20);
-
-
-        var groups = [];
-        var per_group = 1;
-        while(cl.length > 0) {
-            if (cl.length < per_group) {
-                groups.push(cl);
-                break;
-            }
-            groups.push(cl.slice(0, per_group));
-            cl = cl.slice(per_group);
-        }
-
-        var source = $("#cluster-template").html();
-        var template = Handlebars.compile(source);
-
-        $( "#cluster-holder" ).html( template({"groups": groups, "update_time": resp["update_time"]}) );
         } catch (e){
             console.log(e);
         }
