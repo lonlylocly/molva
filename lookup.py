a110f3bcf6a16631e8e07ce5a9a0f2e4674974d5
Nikolay Vasiliev <7804425+lonlylocly@users.noreply.github.com>
2015-02-08 06:37:14 +0300

    update lookup.py

--- lookup.py
+++ lookup.py
@@ -4,71 +4,187 @@ import sys
 import os
 import logging, logging.config
 import json
-import random
 import codecs
+import argparse
+import re
+from datetime import date, timedelta
 
 import stats
 import util
 
-sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
 logging.config.fileConfig("logging.conf")
 
-settings = {} 
-try:
-    settings = json.load(open('global-settings.json', 'r'))
-except Exception as e:
-    logging.warn(e)
+class Word:
 
-DB_DIR = settings["db_dir"] if "db_dir" in settings else os.environ["MOLVA_DIR"]
+    def __init__(self, word=None, word_md5=None, lemma=None, lemma_md5=None):
+        self.word = word
+        self.word_md5 = word_md5
+        self.lemma = lemma
+        self.lemma_md5 = lemma_md5
+    
+    def __str__(self):
+        j = {"word": self.word, "word_md5": self.word_md5, "lemma": self.lemma, "lemma_md5": self.lemma_md5}
+        #return "word=%s; word_md5=%s; lemma=%s; lemma_md5=%s" % (self.word, self.word_md5, self.lemma, self.lemma_md5)
+        return json.dumps(j, indent=2, ensure_ascii=False)
 
-BLOCKED_NOUNS_LIST = u"\n".join(list(u"абвгдеёжзиклмнопрстуфхцчшщыьъэюя"))
+class Tweet:
 
-BLOCKED_NOUNS = ",".join(map( lambda x: str(util.digest(x)), BLOCKED_NOUNS_LIST.split("\n")))
+    def __init__(self, text, tw_id):
+        self.text = text
+        self.words = []
+        self.tw_id = tw_id
+    
+    def __str__(self):
+        j = {"text": self.text, "words": self.words}
 
-NOUNS_LIMIT = 2000
-POST_MIN_FREQ = 10
+        return json.dumps(j, indent=2, ensure_ascii=False)
 
-exp = {
-842505703: "путин",
-3422640972: "россия",
-3811899140: "обама",
-2557218689: "сша",
-3156789482: "мама",
-848893569: "бабушка",
-1368718747: "папа",
-3089392148: "дедушка",
-4078795067: "доллар"
-3462252606: "расти",
-3475785412: "рубль",
-851491985: "падать"
-}
+def get_words_from_query(query):
+    tokens = re.split('\s+', query)
+
+    words = []
+
+    for t in tokens:
+        w = Word(t)
+        w.word_md5 = util.digest(t)
+        words.append(w)
+
+    return words
+
+def get_word_source(cur, word):
+    cur.execute("""
+        select t.noun_md5, s.noun
+        from tweets_words t
+        inner join sources s
+        on t.source_md5 = s.noun_md5
+        where t.source_md5 = %s
+        limit 1
+    """ % (word.word_md5))
+    l = cur.fetchone()
+    if l is not None:
+        word.lemma_md5 = l[0]
+        word.lemma = l[1]
+        return
+
+    cur.execute("""
+        select noun_md5, noun 
+        from nouns 
+        where noun_md5 = %s
+    """ % (word.word_md5))
+    l = cur.fetchone()
+    if l is not None:
+        word.lemma_md5 = l[0]
+        word.lemma = l[1]
+        return
 
 
-def main():
-    logging.info(u"По-русски")
-    parser = util.get_dates_range_parser()
+def fill_lemmas(cur, words):
+    for word in words:
+        get_word_source(cur, word)
+    
+     
+def print_list(l):
+    for i in l:
+        logging.debug(i.__str__())
+    
+
+def get_related_tweets(cur, words):
+    word_md5s = set()
+    for w in words:
+        if w.word_md5 is not None:
+            word_md5s.add(str(w.word_md5))
+        if w.lemma_md5 is not None:
+            word_md5s.add(str(w.lemma_md5))
+    word_md5s = list(word_md5s)
+
+    tweets = {}
+
+    cur.execute("""
+        select n.id, n.noun_md5, t.tw_text
+        from tweets_nouns n
+        inner join tweets t
+        on n.id = t.id
+        where noun_md5 in (%s)
+    """ % ",".join(word_md5s) )
+
+    r = cur.fetchall()
+
+    for l in r:
+        tw_id, noun_md5, tw_text = l
+        if tw_id not in tweets:
+            tweets[tw_id] = Tweet(tw_text, tw_id)
+
+        tweets[tw_id].words.append(noun_md5)
+    
+    return tweets 
+
+def lookup_two_days(cur1, cur2, words):
+
+    tw1 = get_related_tweets(cur1, words)
+    tw2 = get_related_tweets(cur2, words)
+
+    for tw_id in tw1:
+        tw2[tw_id] = tw1[tw_id]
+
+    return tw2
+    
+def main2():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--dir")
+    parser.add_argument("--query")
+
     args = parser.parse_args()
 
-    cur = stats.get_cursor("%s/tweets_%s.db" % (DB_DIR, args.start ))
+    words = get_words_from_query(args.query.decode('utf8'))
+
+    print_list(words)
+
+    if args.db is None:
+        print "Need --db" 
+        return
+    
+    cur = stats.get_cursor(args.db)
+    
+    fill_lemmas(cur, words)
+
+    print_list(words)
+
+    tweets = lookup_two_days(cur, cur, words)
+    for t in sorted(tweets.keys(), key=lambda x: len(tweets[x].words), reverse=True)[:10]:
+        print tweets[t].__str__()
+
+    return
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--dir")
+    args = parser.parse_args()
+
+    today=date.today().strftime('%Y%m%d')
+    ystd=(date.today() - timedelta(1)).strftime('%Y%m%d')
+
+
+    cur_display = stats.get_cursor("%s/tweets_display.db" % args.dir) 
+    cur1 = stats.get_cursor("%s/tweets_%s.db" % (args.dir, today))
+    cur2 = stats.get_cursor("%s/tweets_%s.db" % (args.dir, ystd))
    
-    sims = open(args.end,'r')
-    nouns = stats.get_nouns(cur)
+    cur_display.execute("""
+        select cluster_date, cluster from clusters
+        order by cluster_date desc
+        limit 1
+    """) 
 
-    #valid = map(str,[3615091692, 3577221357, 3504712731  ])
-    #sim_keys = []
-    #while True:
-    #    l = sims.readline()
-    #    if l is None or l == '':
-    #        break
-    #    n1, n2, sim = l.split(';')
-    #    #if n1 in valid and n2 in valid:
-    #    #    print u"%s %s %s" % (nouns[int(n1)], nouns[int(n2)], float(sim.replace(',','.')))
-    #    sim_keys.append(nouns[int(n1)])
-    #    sim_keys.append(nouns[int(n2)])
+    d, c = cur_display.fetchone()
+    c = json.loads(c)
 
-    #for s in set(sim_keys):
-    #    print s
+    words = map(lambda x: Word(word_md5=x["id"]), c[0]["members"])
+
+    tweets = lookup_two_days(cur, cur, words)
+    for t in sorted(tweets.keys(), key=lambda x: len(tweets[x].words), reverse=True)[:10]:
+        print tweets[t].__str__()
 
     return
 
