965d5a2d532ad0882d6e5d8f5f0be0f593d2782e
Nikolay Vasiliev <lonlylocly@yandex.ru>
2015-04-22 03:24:45 +0300

    [#29] commit task.py; update settings.json

--- task.py
+++ task.py
@@ -12,6 +12,7 @@ logging.config.fileConfig("logging.conf")
 SCRIPTDIR="/home/lonlylocly/woape"
 RUNDIR="/home/lonlylocly/run"
 SIMMER_JAR="Simmer-1.0-SNAPSHOT-jar-with-dependencies.jar"
+ALIGNER_JAR="aligner-jar-with-dependencies.jar"
 
 @util.time_logger
 def main():
@@ -38,8 +39,11 @@ def main():
     run("%s/trend_new.py  1>> trend.log 2>&1 " % SCRIPTDIR) 
 
     run("python -m cProfile %s/build-clusters.py   -i 10 1>> clusters.log 2>&1 " % SCRIPTDIR) 
-    run("python -m cProfile %s/prepare-aligner.py --clusters clusters_raw.json >> prepare-aligner.log 2>&1" % SCRIPTDIR) 
-    run("python -m cProfile %s/aligner.py --clusters clusters_raw.json 1>> aligner.log 2>&1 " % SCRIPTDIR) 
+    #run("python -m cProfile %s/prepare-aligner.py --clusters clusters_raw.json >> prepare-aligner.log 2>&1" % SCRIPTDIR) 
+    #run("python -m cProfile %s/aligner.py --clusters clusters_raw.json 1>> aligner.log 2>&1 " % SCRIPTDIR) 
+    run("%s/wordstat.py --clusters clusters_raw.json --out-bigram-stats bigram_stats.json 1>> aligner.log 2>&1 " % SCRIPTDIR) 
+    run("java -Xmx600m -jar %s/%s clusters_raw.json bigram_stats.json aligned_clusters.json >> aligner.log 2>&1 " % (SCRIPTDIR, ALIGNER_JAR))
+    run("%s/save_aligned.py --clusters aligned_clusters.json 1>> aligner.log 2>&1 " % SCRIPTDIR) 
     run("%s/lookup.py --dir /home/lonlylocly/streaming/ 1>> lookup.log 2>&1 " % (SCRIPTDIR)) 
 
     logging.info("done")
